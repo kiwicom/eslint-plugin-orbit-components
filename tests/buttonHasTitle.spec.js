4647dd916991f5c44529534be050b86fabca7560
Luděk Vepřek <weprous@gmail.com>
2019-11-21 16:07:16 +0100

    Fix tests, improving button-has-title error message

--- tests/buttonHasTitle.spec.js
+++ tests/buttonHasTitle.spec.js
@@ -1,8 +1,8 @@
 import ruleTester from "./ruleTester";
-import screenReadersButtons from "../src/rules/screenReadersButtons";
+import buttonHasTitle from "../src/rules/buttonHasTitle";
 
-describe("screenReadersButtons", function() {
-  ruleTester.run("screen-readers-buttons", screenReadersButtons, {
+describe("Button has title", function() {
+  ruleTester.run("button-has-title", buttonHasTitle, {
     valid: [
       {
         code: `import Button from "@kiwicom/orbit-components/lib/Button";
@@ -48,28 +48,28 @@ describe("screenReadersButtons", function() {
         code: `import Button from "@kiwicom/orbit-components/lib/Button";\
                const App = () => <Button iconLeft="something" />`,
         errors: [
-          "Property children or title property is missing on Button. Use title property to add aria-label to be accessible for screen readers.",
+          "Button doesn't have children. Please provide title property to add aria-label, so it's accessible for screen readers.",
         ],
       },
       {
         code: `import { Button as OrbitButton } from "@kiwicom/orbit-components/";\
                const App = () => <OrbitButton fullWidth />`,
         errors: [
-          "Property children or title property is missing on OrbitButton. Use title property to add aria-label to be accessible for screen readers.",
+          "OrbitButton doesn't have children. Please provide title property to add aria-label, so it's accessible for screen readers.",
         ],
       },
       {
         code: `import ButtonLink from "@kiwicom/orbit-components/lib/ButtonLink";\
                const App = () => <ButtonLink iconLeft="something" />`,
         errors: [
-          "Property children or title property is missing on ButtonLink. Use title property to add aria-label to be accessible for screen readers.",
+          "ButtonLink doesn't have children. Please provide title property to add aria-label, so it's accessible for screen readers.",
         ],
       },
       {
         code: `import { ButtonLink as OrbitButtonLink } from "@kiwicom/orbit-components/";\
                const App = () => <OrbitButtonLink fullWidth />`,
         errors: [
-          "Property children or title property is missing on OrbitButtonLink. Use title property to add aria-label to be accessible for screen readers.",
+          "OrbitButtonLink doesn't have children. Please provide title property to add aria-label, so it's accessible for screen readers.",
         ],
       },
       {
@@ -81,7 +81,7 @@ describe("screenReadersButtons", function() {
                   return <Button {...rest} />
                }`,
         errors: [
-          "Property children or title property is missing on Button. Use title property to add aria-label to be accessible for screen readers.",
+          "Button doesn't have children. Please provide title property to add aria-label, so it's accessible for screen readers.",
         ],
       },
       {
@@ -93,7 +93,7 @@ describe("screenReadersButtons", function() {
                   return <Button {...rest} />
                }`,
         errors: [
-          "Property children or title property is missing on Button. Use title property to add aria-label to be accessible for screen readers.",
+          "Button doesn't have children. Please provide title property to add aria-label, so it's accessible for screen readers.",
         ],
       },
     ],
